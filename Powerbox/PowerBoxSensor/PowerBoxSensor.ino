c2dae42ad969fc25cad9aa7a2d3e15a14a0e2a45
Herman <herman@ccme.nl>
2017-01-16 07:42:35 +0100

    Updated powerbox sensor

--- Powerbox/PowerBoxSensor/PowerBoxSensor.ino
+++ Powerbox/PowerBoxSensor/PowerBoxSensor.ino
@@ -12,17 +12,22 @@
 
 #define SPORT_HEADER_DATA 0x10
 #define SPORT_HEADER_DISCARD 0x00
-//#define SPORT_HEADER_RESPONSE 0x32
-//#define SPORT_HEADER_WORKING 0x20
-//#define SPORT_HEADER_IDLE 0x21
 
 #define SENSOR_PHYSICAL_ID 0x11
 
 #define SENSOR_APPL_ID_RB1 0x0B00
 #define SENSOR_APPL_ID_RB2 0x0B10
+#define SENSOR_APPL_ID_RBMAH 0x0B30
+#define SENSOR_APPL_ID_RX1ERR 0x5130
+#define SENSOR_APPL_ID_RX2ERR 0x5140
+#define SENSOR_APPL_ID_RXFLS 0x5150
+#define SENSOR_APPL_ID_RXHLDS 0x5160
 
+#define SENSOR_APPL_ID_GPS 0x0800
 #define SENSOR_APPL_ID_GSPD 0x0830
 #define SENSOR_APPL_ID_GALT 0x0820
+#define SENSOR_APPL_ID_GDIST 0x5110
+#define SENSOR_APPL_ID_GTRAV 0x5120
 
 SoftwareSerial sport(SPORT_IN, SPORT_OUT, true);
 
@@ -37,9 +42,13 @@ byte buffer_index = 0;
 short tele_counter = 0;
 
 //Data holders
+bool gps_enabled, usage_enabled = false;
+
 int batt1Voltage, batt2Voltage = 0;
-int batt1Amps, batt2Amps = 0;
+int batt1Left, batt2Left = 0;
 int gpsSpeed, gpsAltitude, gpsDistance, gpsTravel = 0;
+int rx1Errors, rx2Errors, rxFrameLoss, rxHolds = 0;
+int longHigh, longLow, latHigh, latLow = 0;
 
 //Helper for long / byte conversion
 typedef union {
@@ -56,6 +65,9 @@ void setup() {
 
   //Switch to input, single wire mode
   pinMode(SPORT_IN, INPUT);
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
 
 #if defined(DEBUG)
   debug.begin(115200);
@@ -122,9 +134,12 @@ void loop() {
 
 void handleNewPBFrame(byte frame[4])
 {
-  long value = (frame[2] + (frame[3] * 256)) / 2;
-  byte id = (frame[1] & 0xF0) / 16;
-
+  //Led on indicates data from powerbox
+  digitalWrite(LED_BUILTIN, HIGH);
+  
+  long value = (frame[2] + (frame[3] * 0x100));
+  byte id = (frame[1] & 0xF0) / 0x10;
+  byte type = (frame[1] & 0x0F);
 
 #if defined(DEBUG)
     debug.println("");    
@@ -135,24 +150,77 @@ void handleNewPBFrame(byte frame[4])
     debug.print(value, DEC);
 #endif
 
-
-  if(id == 0x03)
+  if(id == 0x01)
   {
-    batt1Voltage = value;
+    rx1Errors = value;
+  }
+  else if(id == 0x02)
+  {
+    rx2Errors = value;
+  }
+  else if(id == 0x03)
+  {
+    batt1Voltage = value / 2;
   }
   else if(id == 0x04)
   {
-    batt2Voltage = value;
+    batt2Voltage = value  / 2;
   }
-  else if(id == 0x09)
+  else if(id == 0x05)
   {
-    gpsSpeed = value;
+    rxHolds = frame[2];
+    rxFrameLoss = frame[3];
   }
-  else if(id == 0x0A)
+  else if(id == 0x06 && type != 0)
   {
-    gpsAltitude = value;
+    batt1Left = value / 2;
+    usage_enabled = true;    
   }
-  
+  else if(id == 0x07 && type != 0)
+  {
+    batt2Left = value / 2;
+    usage_enabled = true;
+  }
+  else if(id == 0x08)
+  {
+    longHigh = value;
+    gps_enabled = true;
+  }
+  else if(id == 0x09 && type != 0)
+  {
+    gpsSpeed = value / 2;
+    gps_enabled = true;
+  }
+  else if(id == 0x0A && type != 0)
+  {
+    gpsAltitude = value / 2;
+    gps_enabled = true;
+  }
+  else if(id == 0x0B && type != 0)
+  {
+    gpsDistance = value / 2;
+    gps_enabled = true;
+  }
+  else if(id == 0x0C && type != 0)
+  {
+    gpsTravel = value / 2;
+    gps_enabled = true;
+  }
+  else if(id == 0x0D)
+  {
+    longLow = value;
+    gps_enabled = true;
+  }
+  else if(id == 0x0E)
+  {
+    latHigh = value;
+    gps_enabled = true;
+  }
+  else if(id == 0x0F)
+  {
+    latLow = value;
+    gps_enabled = true;
+  }
 }
 
 void handleSportFrame(byte frame[])
@@ -168,23 +236,110 @@ void getSensorFrame(byte data[])
   if(tele_counter == 0)
   {
     applID = SENSOR_APPL_ID_RB1;
-    lh.longValue = (batt1Voltage * 100) + ((batt1Amps * 10) * 256);
+    lh.longValue = batt1Voltage * 100;
   }
   else if(tele_counter == 1)
   {
     applID = SENSOR_APPL_ID_RB2;
-    lh.longValue = (batt2Voltage * 100) + ((batt2Amps * 10) * 256);
+    lh.longValue = batt2Voltage * 100;
   }
-  else if(tele_counter == 2)
+  else if(tele_counter == 2 && usage_enabled)
+  {
+    applID = SENSOR_APPL_ID_RBMAH;
+    lh.longValue = batt1Left + (batt2Left * 0x10000);
+  }
+  else if(tele_counter == 3 && gps_enabled)
   {
     applID = SENSOR_APPL_ID_GSPD;
     lh.longValue = (gpsSpeed / 0.01852);
   }
-  else if(tele_counter == 3)
+  else if(tele_counter == 4 && gps_enabled)
   {
     applID = SENSOR_APPL_ID_GALT;
     lh.longValue = gpsAltitude * 100;
   }
+  else if(tele_counter == 5 && gps_enabled)
+  {
+    applID = SENSOR_APPL_ID_GDIST;
+    lh.longValue = gpsDistance;
+  }
+  else if(tele_counter == 6 && gps_enabled)
+  {
+    applID = SENSOR_APPL_ID_GTRAV;
+    lh.longValue = gpsTravel * 100;
+  }
+  else if(tele_counter == 7)
+  {
+    applID = SENSOR_APPL_ID_RX1ERR;
+    lh.longValue = rx1Errors;
+  }
+  else if(tele_counter == 8)
+  {
+    applID = SENSOR_APPL_ID_RX2ERR;
+    lh.longValue = rx2Errors;
+  }
+  else if(tele_counter == 9)
+  {
+    applID = SENSOR_APPL_ID_RXFLS;
+    lh.longValue = rxFrameLoss;
+  }
+  else if(tele_counter == 10)
+  {
+    applID = SENSOR_APPL_ID_RXHLDS;
+    lh.longValue = rxHolds;
+  }
+//  else if(tele_counter == 11)
+//  {
+//    applID = SENSOR_APPL_ID_GPS;
+//
+//    longHelper cv;
+//    cv.byteValue[0] = highByte(latHigh);
+//    cv.byteValue[1] = lowByte(latHigh);
+//    cv.byteValue[2] = highByte(latLow);
+//    cv.byteValue[3] = lowByte(latLow);
+//    
+//    long coor = cv.longValue;
+//      
+//    if(coor < 0)
+//    {
+//      coor = 0 - coor;          //Negative, make positive
+//      bitClear(coor, 31);       //South
+//      bitSet(coor, 30);
+//    }
+//    else
+//    {
+//      bitClear(coor, 31);       //North
+//      bitClear(coor, 30);
+//    }
+//
+//    lh.longValue = coor;
+//  }
+//  else if(tele_counter == 12)
+//  {
+//    applID = SENSOR_APPL_ID_GPS;
+//
+//    longHelper cv;
+//    cv.byteValue[0] = highByte(longHigh);
+//    cv.byteValue[1] = lowByte(longHigh);
+//    cv.byteValue[2] = highByte(longLow);
+//    cv.byteValue[3] = lowByte(longLow);
+//    
+//    long coor = cv.longValue;
+//
+//    if(coor < 0)
+//    {
+//      coor = 0 - coor;          //Negative, make positive
+//      bitSet(coor, 31);         //West
+//      bitSet(coor, 30);
+//    }
+//    else
+//    {
+//      bitSet(coor, 31);         //East
+//      bitClear(coor, 30);
+//    }
+//
+//    lh.longValue = coor;
+//  }
 
   if(applID == 0)
   {
@@ -201,7 +356,7 @@ void getSensorFrame(byte data[])
   data[5] = lh.byteValue[2];
   data[6] = lh.byteValue[3];
 
-  if(tele_counter >= 10)
+  if(tele_counter >= 12)
   {
     tele_counter = 0;
   }
--- Powerbox/README.md
+++ Powerbox/README.md
@@ -2,3 +2,20 @@
 This sensor provides telemetry data for Powerbox products
 
 Folder PowerBoxSensor contains an Arduino sketch for a Arduino Mini. RX port is connected to the Powerbox telemetry output. RB3 is connected to the S.Port of the X-Series receiver.
+
+This sensor maps the output from a Powerbox to FrSky sensor values. The powerbox must be set to input / telemetry type Futaba or S.Bus.
+
+The following values / sensors are available:
+ - Battery voltage. Voltage is with 0.1 precision for sensors RB1V and RB2V
+ - Battery capacity left (when the powerbox supports it). This shows the mAh left as RB1C and RB2C
+ - Receiver errors: 2 custom sensors with ID 0x5130 and 0x5140
+ - Framelosses: Custom sensor with ID 0x5150
+ - Holds: Custom sensor with ID 0x5160
+ 
+When a GPS is attached to the powerbox the following sensors are availalble:
+ - GSpd: GPS 3D speed
+ - GAlt: GPS height
+ - Distance to home: Custom sensor with id 0x5110 that shows the distance in meters
+ - Distance traveled: Custom sensor with ID 0x5120 that shows the distance traveled in meters (100m precision)
+ 
+When a sensor is not availalble is it not tranismitted and cannot be discovered.
